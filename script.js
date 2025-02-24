28b5227f437dd583133450124ddb9bc3f6579e3e
Chrispippo <netbergamini@gmail.com>
2025-02-24 21:51:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
-  // Aggiungi eventuali funzionalità interattive qui
+  const loginButton = document.getElementById('loginButton');
+  const loginModal = document.getElementById('loginModal');
+  const closeModal = document.querySelector('.modal .close');
+  
+  // Apri il modal al clic del pulsante "Accedi"
+  loginButton.addEventListener('click', () => {
+    loginModal.style.display = 'block';
+  });
+  
+  // Chiudi il modal al clic della "X"
+  closeModal.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+  });
+  
+  // Chiudi il modal cliccando fuori dalla finestra del modal
+  window.addEventListener('click', (event) => {
+    if (event.target === loginModal) {
+      loginModal.style.display = 'none';
+    }
+  });
 });

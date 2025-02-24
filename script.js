d6e3e8123e623af0b04f967a958c83d059c1f669
Chrispippo <netbergamini@gmail.com>
2025-02-24 21:34:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// script.js
-// Al momento non sono necessarie funzionalità JavaScript aggiuntive per il semplice scroll fluido.
-// Puoi aggiungere ulteriori interazioni se necessario.
-document.addEventListener('DOMContentLoaded', () => {
-  // Eventuali script futuri...
-});
+
